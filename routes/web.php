c408f2dacccd152080c1709c5a101d250457b16d
weave.dev <weave.dev1@gmail.com>
2025-06-27 14:38:00 +0800

    chore: removed attendance routes to separate route file

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('attendance.index');
+    return redirect()->route('attendance.cico');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('user-management', action: function () {
+        return Inertia::render('dashboard');
+    })->name('user-management');
 });
 
-Route::group(['as' => 'attendance.', 'prefix' => 'attendance'], function () {
-    Route::get('/', function () {
-        return Inertia::render('attendance/index');
-    })->name('index');
-});
+
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
