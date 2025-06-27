af08ceb31ac0d651b5c1cd67271ebafe549a9a95
weave.dev <weave.dev1@gmail.com>
2025-06-27 13:57:24 +0800

    chore: created migrations for attendance module, and added member id to user

---
+++ database/migrations/2025_06_27_012727_create_attendances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'attendee');
+            $table->datetime('clock_in_time');
+            $table->datetime('clock_out_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
